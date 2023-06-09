28a79c488e78cdf8e0c44899515dbd4760f76b65
Krostal <krostal1991@gmail.com>
2023-06-09 23:20:10 +0800

    Deleted Main.storyboard Created UITabBarController with 2 UINavigationController Created FeedViewController and ProfileViewController

--- Navigation/SceneDelegate.swift
+++ Navigation/SceneDelegate.swift
@@ -10,13 +10,35 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    
+    
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        guard let scene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: scene)
+        
+        let feedViewController = UINavigationController(rootViewController: FeedViewController())
+        feedViewController.tabBarItem = UITabBarItem(title: "Feed", image: UIImage(systemName: "doc.richtext"), tag: 0)
+        feedViewController.view.tintColor = .black
+        
+        
+        let profileViewController = UINavigationController(rootViewController: ProfileViewController())
+        profileViewController.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName: "person.circle"), tag: 1)
+        feedViewController.view.tintColor = .black
+        
+        let tabBarController = UITabBarController()
+        tabBarController.view.tintColor = .black
+        
+        tabBarController.viewControllers = [feedViewController, profileViewController]
+        
+        window.rootViewController = tabBarController
+        
+        window.makeKeyAndVisible()
+        
+        self.window = window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
---
+++ Navigation/ViewControllers/FeedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FeedViewController.swift
+//  Navigation
+//
+//  Created by Konstantin Tarasov on 09.06.2023.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPurple
+  
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
