b3e563f6928c1e7adcf56e1158096334a742d8e9
Krostal <krostal1991@gmail.com>
2023-06-20 00:43:01 +0800

    Changed position of ProfileHeaderView using Auto Layout, added new UIButton on the bottom of safeArea

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -7,19 +7,26 @@ class ProfileViewController: UIViewController {
 
     private var newStatus = ""
     
+    private lazy var newButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("New button", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.tintColor = .white
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(profileHeaderView)
+        profileHeaderView.translatesAutoresizingMaskIntoConstraints = false
+        setupView()
         self.navigationItem.title = "Profile"
         profileHeaderView.backgroundColor = .lightGray
         view.safeAreaLayoutGuide.owningView?.backgroundColor = .white
         profileHeaderView.setupConstraints()
         profileHeaderView.setStatusButton.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
         profileHeaderView.statusTextField.addTarget(self, action: #selector(statusTextChanged(_:)), for: .editingChanged)
-    }
-    
-    override func viewWillLayoutSubviews() {
-        profileHeaderView.frame = view.safeAreaLayoutGuide.layoutFrame
     }
     
     @objc func buttonPressed(_ sender: UIButton) {
@@ -32,6 +39,24 @@ class ProfileViewController: UIViewController {
         if profileHeaderView.statusTextField.text != nil {
             newStatus = profileHeaderView.statusTextField.text!
         }
+    }
+    
+    private func setupView() {
+        let safeAreaGuide = view.safeAreaLayoutGuide
+        
+        view.addSubview(newButton)
+        
+        NSLayoutConstraint.activate([
+            profileHeaderView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            profileHeaderView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            profileHeaderView.topAnchor.constraint(equalTo: safeAreaGuide.topAnchor),
+            profileHeaderView.heightAnchor.constraint(equalToConstant: 220),
+            
+            newButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            newButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            newButton.bottomAnchor.constraint(equalTo: safeAreaGuide.bottomAnchor),
+            newButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
 
 }
