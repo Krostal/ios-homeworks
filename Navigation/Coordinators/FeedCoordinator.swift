f49352cdc5090e4499db410dc1e00da11e548138
Krostal <krostal1991@gmail.com>
2023-08-21 14:23:06 +0700

    set title in PostViewController

--- Navigation/Coordinators/FeedCoordinator.swift
+++ Navigation/Coordinators/FeedCoordinator.swift
@@ -3,11 +3,14 @@ import UIKit
 
 final class FeedCoordinator {
     
+    private let new: News = News(title: "My post")
+    
     var navigationController: UINavigationController?
     
     func showDetails() {
         let postViewController = PostViewController()
         navigationController?.pushViewController(postViewController, animated: true)
+        postViewController.titleNews = new.title
     }
 }
 
